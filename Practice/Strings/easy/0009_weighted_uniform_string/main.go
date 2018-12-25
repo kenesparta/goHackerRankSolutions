dae300e856b3c629e8dfc04bdd33ca34cfa189e5
Ken Esparta Ccorahua <kenesparta@pm.me>
2018-12-25 18:22:27 -0500

    Weighted uniform string SOLVED!

--- Practice/Strings/easy/0009_weighted_uniform_string/main.go
+++ Practice/Strings/easy/0009_weighted_uniform_string/main.go
@@ -15,20 +15,56 @@ import (
 	"strings"
 )
 
+// lowercase 97 -122
+const rest = int(96)
+
 // Complete the weightedUniformStrings function below.
-func weightedUniformStrings(s string, queries []int32) []string {
-	return nil
+func createUniformSubString(s string) (wStr []int) {
+	size := len(s)
+	totalAccSum := 1
+	for i := 0; i < size-1; i++ {
+		if s[i] == s[i+1] {
+			totalAccSum++
+		} else {
+			totalAccSum = 1
+		}
+		value := int(s[i]) - rest
+		wStr = append(wStr, value*totalAccSum)
+	}
+	value := int(s[size-1]) - rest
+	wStr = append(wStr, value)
+	return
+}
+
+func weightedUniformStrings(s string, queries []int32) (resp []string) {
+	unif := createUniformSubString(s)
+	fmt.Println(unif)
+	for _, v1 := range queries {
+		var flag bool
+		for _, v2 := range unif {
+			if v1 == int32(v2) {
+				flag = true
+				break
+			}
+		}
+		if flag {
+			resp = append(resp, "Yes")
+		} else {
+			resp = append(resp, "No")
+		}
+	}
+	return
 }
 
 func main() {
-	reader := bufio.NewReaderSize(os.Stdin, 1024 * 1024)
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
 
 	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
 	checkError(err)
 
 	defer stdout.Close()
 
-	writer := bufio.NewWriterSize(stdout, 1024 * 1024)
+	writer := bufio.NewWriterSize(stdout, 1024*1024)
 
 	s := readLine(reader)
 
@@ -49,7 +85,7 @@ func main() {
 	for i, resultItem := range result {
 		fmt.Fprintf(writer, "%s", resultItem)
 
-		if i != len(result) - 1 {
+		if i != len(result)-1 {
 			fmt.Fprintf(writer, "\n")
 		}
 	}
